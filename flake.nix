5d1532539a496dd650c9aacb28da094e9153dbc5
fractal <fractal@nebula-nook.ru>
2026-02-23 16:46:39 +0300

    fix: arch probably not needed
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

--- flake.nix
+++ flake.nix
@@ -2,11 +2,9 @@
   description = "Nix flake for the kpl project.";
 
   outputs = {...}: {
-    x86_64-linux = {
-      overlays = {
-        vscode = {
-          dynamic-style = import ./nix/vscode/0001-dynamic-style.nix;
-        };
+    overlays = {
+      vscode = {
+        dynamic-style = import ./nix/vscode/0001-dynamic-style.nix;
       };
     };
   };
