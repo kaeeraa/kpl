627ca66c44fad7f285199177b3fd2348e6b23944
fractal <fractal@nebula-nook.ru>
2026-02-23 17:09:18 +0300

    tmp
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

--- nix/vscode/0001-dynamic-style.nix
+++ nix/vscode/0001-dynamic-style.nix
@@ -1,16 +1,15 @@
-{ fetchFromGitHub }: final: prev: {
-  vscode = prev.vscode.overrideAttrs (old: {
+{ pkgs, fetchFromGitHub }:
+
+let
+  myVscode = pkgs.vscode.overrideAttrs (old: {
     src = fetchFromGitHub {
       owner = "microsoft";
       repo = "vscode";
       rev = "1.106.2";
-      sha256 = "";
+      sha256 = "sha256-...";
     };
-
-    unpackPhase = "true";
-
-    patches = (old.patches or []) ++ [../../source/vscode/0001-dynamic-style.patch];
-
+    unpackPhase = ":";
+    patches = (old.patches or []) ++ [./0001-dynamic-style.patch];
     buildPhase = ''
       export NODE_ENV=production
       yarn --frozen-lockfile
@@ -18,4 +17,7 @@
       ./scripts/code.sh compile
     '';
   });
+in
+{
+  vscode = myVscode;
 }
